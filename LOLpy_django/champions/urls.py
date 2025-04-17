01c38c17d43dde62dd6ddbb97815ecf08e06b7e8
Everton Lopes <everton.lopes@zero-defect.com.br>
2025-04-17 13:05:25 -0300

    fix: icon images alt attribute

--- LOLpy_django/champions/urls.py
+++ LOLpy_django/champions/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from . import views
-
 urlpatterns = [
-    path('champions/', views.champions, name='champions'),
+    path('champions/', views.champions, name='champions')
 ]
